d13ee7e07c1e11162ae819ee9993d8984f867c69
sparpana <masprpna@gmail.com>
2026-01-09 11:44:55 -0800

    chore: configure dev server proxy for socket.io

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -3,4 +3,12 @@ const config = require("./webpack.config");
 module.exports = {
   ...config,
   mode: "development",
+  devServer: {
+    proxy: {
+      '/socket.io': {
+        target: 'http://localhost:3000',
+        ws: true
+      }
+    }
+  }
 };
